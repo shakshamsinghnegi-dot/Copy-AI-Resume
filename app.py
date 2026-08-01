b3d07066247945bd6efee3edd1c31cf6afeb2046
shakshamsinghnegi-dot <shakshamsinghnegi@gmail.com>
2026-08-01 11:11:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -63,8 +63,8 @@ def generate_image(img_prompt,slide_no = 1):
 import requests as r
 content=r.get(url).content
 with open(f"ai_image_{slide_no}.jpeg",'wb') as f:
-  f.write(content):
-    return url
+  f.write(content)
+  return url
 
 def run_agent(leader_agent, query):
   prompt = f"""Based on Below given Query, your task is to call specific tool,
